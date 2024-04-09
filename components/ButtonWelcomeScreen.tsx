b1208a84cf7afe09dbc7dfd7887b691dfda06d11
Daniel_Muñoz <127697716+dmunozlieb@users.noreply.github.com>
2024-04-09 20:28:07 +0200

    feat: welcome home has been created (#4)
    Co-authored-by: DanielMuñoz <daniel.munoz116@educa.madrid.org>

---
+++ components/ButtonWelcomeScreen.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+
+interface CustomButtonProps {
+	text: String;
+}
+
+const ButtonWelcomeScreen: React.FC<CustomButtonProps> = ({ text }) => {
+	return (
+		<View style={styles.container}>
+			<TouchableOpacity style={styles.button}>
+				<Text style={styles.buttonText}>{text}</Text>
+			</TouchableOpacity>
+		</View>
+	)
+}
+
+export default ButtonWelcomeScreen
+
+const styles = StyleSheet.create({
+	container: {
+		alignItems: 'center'
+	},
+	button: {
+		backgroundColor: '#3A2635',
+		width: '90%',
+		height: '25%',
+		borderRadius: 10,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	buttonText: {
+		color: 'gray',
+		textAlign: 'center',
+		fontSize: 17,
+		fontStyle: 'italic'
+	}
+})
