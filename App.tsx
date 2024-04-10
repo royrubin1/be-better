0fc3c8c48ac281cfd127c806b4a6a4ed8fa1eab9
Roy Rubin <104563756+royrubin1@users.noreply.github.com>
2024-04-10 19:34:16 +0200

    Feature/issue 1/create new auth screen (#5)
    * Auth Screen Has Been Created
    
    * refactor: dependencies update
    
    * feat: Auth Screen Has Been Added

--- App.tsx
+++ App.tsx
@@ -1,19 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import WelcomeScreen from './screens/WelcomeScreen';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import AuthScreen from "./screens/AuthScreen";
 
 export default function App() {
-  return (
-    <WelcomeScreen />
-  );
+  return <AuthScreen />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
