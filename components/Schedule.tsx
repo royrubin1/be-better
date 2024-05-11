815a5066377a0345490feb342d7302ce7279d690
DanielMuñoz <daniel.munoz116@educa.madrid.org>
2024-05-11 13:48:28 +0200

    Changes on HomeScreen, load tasks when is a day selected and load tasks reminder

--- components/Schedule.tsx
+++ components/Schedule.tsx
@@ -1,26 +1,51 @@
 import React, { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  FlatList,
-  StyleSheet,
-  Image,
-  ListRenderItem,
-} from "react-native";
+import { View, Text, FlatList, StyleSheet, Image } from "react-native";
 import moment from "moment";
 import DateSection from "./DateSection";
 import { auth, db } from "../config/firebase";
+import { uid } from "uid";
 import { collection, getDocs, query, where } from "firebase/firestore";
-import { Item } from "react-native-picker-select";
 
 const Schedule = () => {
   const currentDate = moment();
   const [selectedDay, setSelectedDay] = useState(currentDate.date());
   const [tasksByDay, setTasksByDay] = useState({});
+  const [reminderTasks, setReminderTasks] = useState([]);
 
   useEffect(() => {
-    fetchUserTasks();
+    const fetchAndFilterUserTasks = async () => {
+      const goals = await fetchUserTasks();
+      const filterGoals = goals.filter((goal) => {
+        const selectedDate = currentDate.date(selectedDay);
+        const selectedDayMilliseconds = selectedDate.valueOf();
+        // Check if the current date is within the range
+        return (
+          goal.start_date >= selectedDayMilliseconds &&
+          goal.end_date <= selectedDayMilliseconds
+        );
+      });
+      setTasksByDay({ ...tasksByDay, [selectedDay]: filterGoals });
+    };
+    fetchAndFilterUserTasks();
   }, [selectedDay]);
+
+  useEffect(() => {
+    const fetchAndFilterUserTasks = async () => {
+      const goals = await fetchUserTasks();
+      const filterGoalsReminder = goals.filter((goal) => {
+        const tomorrowDate = moment(currentDate)
+          .add(1, "day")
+          .startOf("day")
+          .format("YYYY-MM-DD");
+        const goalStartDate = moment(goal.start_date)
+          .startOf("day")
+          .format("YYYY-MM-DD");
+        return goalStartDate.trim() === tomorrowDate.trim();
+      });
+      setReminderTasks(filterGoalsReminder);
+    };
+    fetchAndFilterUserTasks();
+  }, []);
 
   const fetchUserTasks = async () => {
     // get all tasks with uid user auth
@@ -34,16 +59,7 @@ const Schedule = () => {
     querySnapShot.forEach((doc) => {
       goals.push({ id: doc.id, ...doc.data() });
     });
-    const filterGoals = goals.filter((goal) => {
-      const selectedDate = currentDate.date(selectedDay);
-      const selectedDayMilliseconds = selectedDate.valueOf();
-      // Check if the current date is within the range
-      return (
-        goal.start_date >= selectedDayMilliseconds &&
-        goal.end_date <= selectedDayMilliseconds
-      );
-    });
-    setTasksByDay({ ...tasksByDay, [selectedDay]: filterGoals });
+    return goals;
   };
 
   // Category default with their colors
@@ -96,7 +112,8 @@ const Schedule = () => {
         >
           <Text style={{ color: "#D4D4F6" }}>{item.title}</Text>
           <Text style={{ color: "#D4D4F6" }}>
-            {item.startTime} - {item.endTime}
+            {moment(item.start_date).format("DD-MM")} -{" "}
+            {moment(item.end_date).format("DD-MM")}
           </Text>
         </View>
       </View>
@@ -127,7 +144,7 @@ const Schedule = () => {
         <Text style={styles.reminderText}>
           Don't forget schedule for tomorrow
         </Text>
-        {!tasksByDay[selectedDay] || [] ? (
+        {!reminderTasks || [] ? (
           <View
             style={{
               display: "flex",
@@ -143,7 +160,7 @@ const Schedule = () => {
           </View>
         ) : (
           <FlatList
-            data={tasksByDay[selectedDay] || []}
+            data={reminderTasks || []}
             showsVerticalScrollIndicator={false}
             renderItem={renderTasks}
             style={{ height: "100%" }}
