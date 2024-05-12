a4d0d1c45ba42c3ba965ccc8f1f9925f6e95a4ac
royrubin1 <royrubin@protonmail.com>
2024-05-12 23:14:07 +0200

    Settings menu has been added

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,14 +1,49 @@
-import { StyleSheet, Text, View } from "react-native";
+import { signOut } from "firebase/auth";
 import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { auth } from "../config/firebase";
 
-const SettingsScreen = () => {
+const SettingsScreen = ({ navigation }) => {
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        navigation.navigate("Auth");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Settings</Text>
+
+      <Button title="Log Out" onPress={handleLogout} />
     </View>
   );
 };
 
-export default SettingsScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  option: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 10,
+    width: "100%",
+  },
+  optionText: {
+    fontSize: 18,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default SettingsScreen;
